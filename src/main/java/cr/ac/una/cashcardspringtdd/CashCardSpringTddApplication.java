ef72d49592052e9e98e2a48a75d16fb62dc14fa3
Jason Mora <jeisonmora0115@gmail.com>
2024-04-16 15:13:35 -0600

    Project started

---
+++ src/main/java/cr/ac/una/cashcardspringtdd/CashCardSpringTddApplication.java
@@ -0,0 +1,13 @@
+package cr.ac.una.cashcardspringtdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashCardSpringTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashCardSpringTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cr/ac/una/cashcardspringtdd/CashCardSpringTddApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.ac.una.cashcardspringtdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashCardSpringTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
